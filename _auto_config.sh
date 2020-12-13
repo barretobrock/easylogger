6e24ded6916181e2eab8135f7810af1e2f4c381a
Bobrock <barret.obrock@gmail.com>
2020-12-12 21:23:42 -0600

    Add package management scripts

---
+++ _auto_config.sh
@@ -0,0 +1,9 @@
+# **This is an automatically-constructed file. See config.py for the source.**
+
+REPO_NAME=easylogger
+REPO_DIR=/home/bobrock/extras/easylogger
+GIT_URL=git+https://github.com/barretobrock/easylogger.git#egg=easylogger
+VENV_PATH=${HOME}/venvs/easylogger/bin/python3
+DEP_LINKS=(
+)
+MAIN_BRANCH=main
