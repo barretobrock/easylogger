98f6f8801360216fa415a7fa42917be60dd9de73
Bobrock <barret.obrock@gmail.com>
2020-12-12 21:25:07 -0600

    Add setup.py changes

--- setup.py
+++ setup.py
@@ -5,19 +5,14 @@ Resources to build this:
 """
 from setuptools import setup, find_packages
 import versioneer
+from config import config_dict
 
 
 setup_args = {
-    'name': 'easylogger',
     'version': versioneer.get_version(),
     'cmdclass': versioneer.get_cmdclass(),
-    'license': 'MIT',
-    'description': 'A relatively simple logging configuration with just enough customization '
-                   'for my projects\' needs.',
-    'url': 'https://github.com/barretobrock/easylogger',
-    'author': 'Barret Obrock',
-    'author_email': 'bobrock@tuta.io',
     'packages': find_packages(exclude=['tests'])
 }
+setup_args.update(config_dict)
 
 setup(**setup_args)
